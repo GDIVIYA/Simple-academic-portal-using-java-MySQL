a93225cdaf80f636c5e49117a24c4bcdf47f8462
GDIVIYA <112324368+GDIVIYA@users.noreply.github.com>
2025-11-16 15:10:35 +0530

    Clean up DBConnection.java comments and code
    Removed comments and unnecessary code from DBConnection.java.

--- com/project/DBConnection.java
+++ com/project/DBConnection.java
@@ -8,19 +8,11 @@ public class DBConnection {
     // 1. Database Connection Parameters
     private static final String URL = "jdbc:mysql://localhost:3306/students_db";
     private static final String USER = "root";
-    // NOTE: Replace 'YourNewPassword123!' with the password you successfully reset!
     private static final String PASSWORD = "YourNewPassword123!"; 
 
-    /**
-     * Establishes and returns a connection to the MySQL database.
-     * @return Connection object
-     * @throws SQLException 
-     */
     public static Connection getConnection() throws SQLException {
         try {
-            // Optional: Loading the driver is not strictly necessary for newer JDBC versions 
-            // but is good practice for compatibility.
-            // Class.forName("com.mysql.cj.jdbc.Driver");
+           
             
             // 2. Establish the connection using DriverManager
             return DriverManager.getConnection(URL, USER, PASSWORD);
